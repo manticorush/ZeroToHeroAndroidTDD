d2650e36cfaf5f1d187b4c4a519e2b274941d309
Lufavetskaya <lufavetzakh@gmail.com>
2024-01-02 19:24:36 +0300

    task/009-remove_text not solved with "GONE"

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,40 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.titleTextView)
+        val removeButton: Button = findViewById(R.id.removeButton)
+
+        removeButton.setOnClickListener {
+            textView.visibility = View.GONE
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY_FOR_TEXTVIEW_VISIBILITY, textView.visibility)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        textView.visibility = savedInstanceState.getInt(KEY_FOR_TEXTVIEW_VISIBILITY)
+    }
+
+    companion object{
+        const val KEY_FOR_TEXTVIEW_VISIBILITY = "visibilityKey"
     }
 }
+
